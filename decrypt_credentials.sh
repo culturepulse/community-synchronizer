988852a3cf2d8f1e1a67a83171cec34807699265
adam.zurek <adam.zurek@backbone.sk>
2022-12-06 09:18:05 +0100

    feat: decrypt google sheets credentials

---
+++ decrypt_credentials.sh
@@ -0,0 +1,4 @@
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output credentials.json credentials.json.gpg
